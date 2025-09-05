c324c055c8dffe26e0143940ebdda58f691855dd
Rohit <kumarrohit67476@gmail.com>
2025-09-05 19:02:44 +0530

    Jenkinsfile

--- backend/__init__.py
+++
@@ -1,9 +0,0 @@
-import joblib
-import os
-
-MODEL_PATH = os.environ.get("MODEL_PATH", "/app/model.pkl")
-
-def load_model():
-    # use joblib for scikit-learn objects (faster + safer)
-    model = joblib.load(MODEL_PATH)
-    return model
